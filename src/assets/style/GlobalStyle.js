55ed4411bfa23d139833dfa61dde11cdad2832a2
Nezertiam <mawyn.nhek@gmail.com>
2022-01-04 11:59:10 +0100

    [ADD] Global style and Bootstrap

---
+++ src/assets/style/GlobalStyle.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+
+    /* 
+        Sets the footer at the bottom page or
+        at the bottom of viewport if page height
+        doesn't pass viewport height
+    */
+    html, body, #root {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    #root {
+        display: table;
+        width: 100%;
+    }
+    #footer {
+        display: table-row;
+        width: 100%;
+        height: 100px;
+        z-index: 10;
+    }
+    /* -------------------------------------- */
+
+`
+
+export default GlobalStyle;
